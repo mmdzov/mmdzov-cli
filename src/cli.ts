802f51f9438eb840dd36d93eabe707598bc070ba
mmdzov <mzov939@gmail.com>
2022-01-30 01:35:49 +0330

    Added new Command

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,7 @@
 
 import chalk from "chalk";
 import yargs from "yargs";
+import shell from "shelljs";
 
 const cli = yargs;
 
@@ -10,6 +11,17 @@ cli.scriptName("mmdzov");
 cli.command("$0", "main description cli", (yrg) => {
   console.log(chalk.blue("try mmdzov -h to more help"));
 });
+
+cli.command(
+  "$0 use ten-stack <repo-name>",
+  "install ten-stack-starter",
+  (yrg) => {},
+  (args) => {
+    shell.exec(
+      `degit https://github.com/mytls/ten-stack-starter.git ${args.repoName}`
+    );
+  }
+);
 
 cli.help("h").alias("h", "help");
 
